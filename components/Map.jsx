02d880cb916aa6f417b12a0bff2d5fe36f58c88d
LeMinhTuong <leminhtuong09122002@gmail.com>
2023-11-25 14:44:26 +0700

    update

--- components/Map.jsx
+++ components/Map.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import L from "leaflet";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 
 import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
@@ -9,6 +9,8 @@ import markerIcon from "leaflet/dist/images/marker-icon.png";
 import markerShadow from "leaflet/dist/images/marker-shadow.png";
 import "leaflet/dist/leaflet.css";
 import Flag from "react-world-flags";
+import { OpenStreetMapProvider, GeoSearchControl } from "leaflet-geosearch";
+import "node_modules/leaflet-geosearch/dist/geosearch.css";
 
 // @ts-ignore
 delete L.Icon.Default.prototype._getIconUrl;
@@ -19,33 +21,57 @@ L.Icon.Default.mergeOptions({
 });
 
 function Map({ center, locationValue }) {
+  const [showSearchControl, setShowSearchControl] = useState(false);
+  useEffect(() => {
+    const map = L.map("map");
+
+    // Initialize the map with or without the search control based on showSearchControl state
+    const searchControl = new GeoSearchControl({
+      provider: new OpenStreetMapProvider(),
+      style: "bar",
+    });
+    if (showSearchControl) {
+      map.addControl(searchControl);
+    }
+
+    return () => {
+      map.remove();
+    };
+  }, [showSearchControl]);
+
+  useEffect(() => {
+    // Show the search control when the map is mounted
+    setShowSearchControl(true);
+  }, []);
   return (
-    <MapContainer
-      center={center || [51, -0.09]}
-      zoom={center ? 4 : 2}
-      scrollWheelZoom={false}
-      className="h-[35vh] rounded-lg"
-    >
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMapa> contributors'
-      />
-      {locationValue ? (
-        <>
-          {center && (
-            <Marker position={center}>
-              <Popup>
-                <div className="flex justify-center items-center animate-bounce">
-                  <Flag code={locationValue} className="w-10" />
-                </div>
-              </Popup>
-            </Marker>
-          )}
-        </>
-      ) : (
-        <>{center && <Marker position={center} />}</>
-      )}
-    </MapContainer>
+    <div id="map">
+      <MapContainer
+        center={center || [51, -0.09]}
+        zoom={center ? 4 : 2}
+        scrollWheelZoom={false}
+        className="h-[35vh] rounded-lg"
+      >
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMapa> contributors'
+        />
+        {locationValue ? (
+          <>
+            {center && (
+              <Marker position={center}>
+                <Popup>
+                  <div className="flex justify-center items-center animate-bounce">
+                    <Flag code={locationValue} className="w-10" />
+                  </div>
+                </Popup>
+              </Marker>
+            )}
+          </>
+        ) : (
+          <>{center && <Marker position={center} />}</>
+        )}
+      </MapContainer>
+    </div>
   );
 }
 
--- components/navbar/UserMenu.jsx
+++ components/navbar/UserMenu.jsx
@@ -73,7 +73,7 @@ function UserMenu({ authState, loggedUser }) {
       }}
     >
       <div className="flex flex-row items-center gap-6">
-        {loggedUser.role !== 3 && (
+        {loggedUser.role === 2 && (
           <div
             className="hidden md:block text-sm font-semibold py-3 rounded-full hover:bg-neutral-100 transition cursor-pointer"
             onClick={onRent}
