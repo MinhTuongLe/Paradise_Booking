ce70bb8f405ea108fd6698a94a1b9a225fb4bfe8
LeMinhTuong <leminhtuong09122002@gmail.com>
2023-12-09 11:46:31 +0700

    revert api

--- const.js
+++ const.js
@@ -3,8 +3,8 @@ import { MdCancel, MdIncompleteCircle, MdPending } from "react-icons/md";
 
 export const BASE_URL = "http://localhost:3000";
 export const API_URL_LOCAL = "http://localhost:8081/api/v1";
-// export const API_URL = "https://paradisebookingapp.up.railway.app/api/v1";
-export const API_URL = "http://47.128.64.99:8081/api/v1";
+export const API_URL = "https://paradisebookingapp.up.railway.app/api/v1";
+// export const API_URL = "http://47.128.64.99:8081/api/v1";
 export const LIMIT = 20;
 
 export const booking_status = [
